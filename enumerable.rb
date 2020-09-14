7647f8158634a37da43433e3957053c495d117ab
dasileker <dasileker@gmail.com>
2020-09-14 15:47:20 +0100

    refactored the my_any & my_none beside adding any & none

--- enumerable.rb
+++ enumerable.rb
@@ -45,54 +45,75 @@ module Enumerable
   end
 
   def my_any?(&block)
-    !(self.my_none? &block)
+    !(self.my_none?(&block))
+  end
+
+  def my_none?
+    if !(block_given?)
+      self.my_each { |item| return false if item }
+      return true
     end
-    # my_each(arr) do |i|
-    #   if yield(arr[i] == true)
-    #    return true
-    #   else
-    #     return false
-    #   end
-    # end
-  # end
+    self.my_each { |item| return false if (yield(item)) }
+    true
+  end
 end
 
 include Enumerable
-array1 = ['hi', 34, 'potatoes', 'horses', 33]
+array1 = ["hi", 34, "potatoes", "horses", 33]
 
-puts "\nmy_each output\:"; puts ''
+puts "\nmy_each output\:"; puts ""
 array1.my_each { |item| puts item }
 
-puts "\neach output\:"; puts ''
+puts "\neach output\:"; puts ""
 array1.each { |item| puts item }
 
-puts "\nmy_each_with_index output\:"; puts ''
+puts "\nmy_each_with_index output\:"; puts ""
 array1.my_each_with_index { |item, index| puts "#{item} : #{index} " }
 
-puts "\neach_with_index output\:"; puts ''
+puts "\neach_with_index output\:"; puts ""
 array1.each_with_index { |item, index| puts "#{item} : #{index} " }
 
-puts "\nmy_select output\:"; puts ''
+puts "\nmy_select output\:"; puts ""
 puts array1.my_select { |item| item.to_s.length > 2 }
 
-puts "\nselect output\:"; puts ''
+puts "\nselect output\:"; puts ""
 puts array1.select { |item| item.to_s.length > 2 }
 
-puts ''; puts "my_all? output\:"; puts ''
+puts ""; puts "my_all? output\:"; puts ""
 puts %w[lul what potatoes uhh].my_all? { |word| word.length >= 3 }
-puts ['lul', 'what', 'potatoes', 'uhh', nil].my_all?
+puts ["lul", "what", "potatoes", "uhh", nil].my_all?
 
-puts ''; puts "all? output\:"; puts ''
+puts ""; puts "all? output\:"; puts ""
 puts %w[lul what potatoes uhh].all? { |word| word.length >= 3 }
-puts ['lul', 'what', 'potatoes', 'uhh', nil].all?
+puts ["lul", "what", "potatoes", "uhh", nil].all?
 
-puts "\nmy_any? output\:"; puts ''
+puts ""; puts "\nany? output\:"; puts ""
+puts ["ant", "bear", "cat"].any? { |word| word.length >= 3 }
+puts ["ant", "bear", "cat"].any? { |word| word.length >= 4 }
+puts [].any?
+puts [nil].any?
+puts [nil,false].any?
 
-puts %w[ant bear cat].any? { |word| word.length >= 3 } #=> true
-puts %w[ant bear cat].any? { |word| word.length >= 4 } #=> true
-puts %w[ant bear cat].any?(/d/) #=> false
-puts [nil, true, 99].any?(Integer) #=> true
-puts [nil, true, 99].any? #=> true
-puts [].any? #=> false
+puts "";puts "\nmy_any? output\:";puts ""
+puts ["ant", "bear", "cat"].my_any? {|word| word.length >= 3}   
+puts ["ant", "bear", "cat"].my_any? {|word| word.length >= 4}   
+puts [].my_any?
+puts [nil].my_any?
+puts [nil,false].my_any?
+
+
+puts "";puts "\nmy_none? output\:";puts ""
+puts %w{ant bear cat}.my_none? {|word| word.length == 5}  
+puts %w{ant bear cat}.my_none? {|word| word.length >= 4}
+puts [].my_none?                                          
+puts [nil].my_none?                                       
+puts [nil,false].my_none?
+
+puts "";puts "\nnone? output\:";puts ""
+puts %w{ant bear cat}.none? {|word| word.length == 5}  
+puts %w{ant bear cat}.none? {|word| word.length >= 4}
+puts [].none?                                          
+puts [nil].none?                                       
+puts [nil,false].none?
 
 # rubocop : enable  Style/Semicolon, Lint/AmbiguousBlockAssociation, Style/MixinUsage
