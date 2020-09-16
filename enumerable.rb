6c88c1864fb5ccc878748c33724ca88c45d45724
dasileker <dasileker@gmail.com>
2020-09-16 10:41:53 +0100

    refactored my_each

--- enumerable.rb
+++ enumerable.rb
@@ -2,9 +2,10 @@
 
 module Enumerable
   def my_each
-    return to_enum unless block_given?
-
-    length.times { |index| yield(self[index]) }
+    for item in self 
+      yield(item)
+    end
+    self
   end
 
   def my_each_with_index
